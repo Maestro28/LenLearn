5502cea69585a7e23d8f062cd58b48ed4b193790
Maestro28 <bohdan.rybak28@gmail.com>
2021-12-15 07:25:33 +0200

    Pagination posts and translations

--- app/routes.py
+++ app/routes.py
@@ -62,8 +62,15 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/vocabulary_practice', methods=['GET', 'POST'])
 @login_required
@@ -82,7 +89,6 @@ def voca_pract():
 @login_required
 def voca_add():
     form = VokaAddForm()
-    translations = current_user.translations.all()
     if form.validate_on_submit():
         v = Vocabular(text=form.text.data, translate=form.translation.data,
                       owner=current_user)
@@ -90,7 +96,16 @@ def voca_add():
         db.session.commit()
         flash('One more word added')
         return redirect(url_for('voca_add'))
-    return render_template('voca_add.html', title="Add", form=form, translations=translations)
+    #translations = current_user.translations.all()
+    page = request.args.get('page', 1, type=int)
+    translations = current_user.translations.paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('voca_add', page=translations.next_num) \
+        if translations.has_next else None
+    prev_url = url_for('voca_add', page=translations.prev_num) \
+        if translations.has_prev else None
+    return render_template('voca_add.html', title="Add", form=form, translations=translations.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -126,6 +141,13 @@ def blog():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('blog'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    #posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('blog', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('blog', page=posts.prev_num) \
+        if posts.has_prev else None
     return render_template("blog.html", title='blog', form=form,
-                           posts=posts)
+                           posts=posts.items, next_url=next_url, prev_url=prev_url)
