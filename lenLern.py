eb9720f73d72aa73c7c4a0d915d341a3043ac942
Maestro28 <bohdan.rybak28@gmail.com>
2022-06-20 19:07:27 +0300

    postgre

--- lenLern.py
+++ lenLern.py
@@ -1,7 +1,21 @@
 from app import app, db
 from app.models import User, Post
+from flask.cli import FlaskGroup
 
 @app.shell_context_processor
 def make_shell_context():
     # don't work
     return {'db': db, 'User': User, 'Post': Post}
+
+
+cli = FlaskGroup(app)
+
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+if __name__ == "__main__":
+    cli()
